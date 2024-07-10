5a673e371669658b1aba3b7b769d67060417601c
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-10 15:27:09 +0200

    Added write for testing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
-int main(int argc, char const *argv[])
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 15:25:44 by danjimen          #+#    #+#             */
+/*   Updated: 2024/07/10 15:26:40 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int	main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+	(void)argc;
+	(void)argv;
+	write(1, "Rodrigo\n", 8);
+	return (0);
 }
