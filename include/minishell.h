883ee58b11367267d4adda991870b8a65fb437e8
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-10 15:24:42 +0200

    Added libft. Added minishell.h template

---
+++ include/minishell.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 07:52:36 by danjimen          #+#    #+#             */
+/*   Updated: 2024/07/10 15:18:07 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// DEFINES
+
+//libft
+#include "include/libft/libft.h"
+
+// printf, readline, perror
+#include <stdio.h>
+
+// rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay, add_history
+#include <readline/readline.h>
+
+// malloc, free, exit, getenv
+#include <stdlib.h>
+
+// write, access, read, close, fork, getcwd, chdir, unlink, execve, dup, dup2,
+// pipe, isatty, ttyname, ttyslot, tcsetattr, tcgetattr
+#include <unistd.h>
+
+// open, stat, lstat, fstat
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// wait, waitpid, wait3, wait4
+#include <sys/wait.h>
+
+// signal, sigaction
+#include <signal.h>
+
+// opendir, readdir, closedir
+#include <sys/types.h>
+#include <dirent.h>
+
+// strerror
+#include <string.h>
+
+// ioctl
+#include <sys/ioctl.h>
+
+// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+#include <curses.h>
+#include <term.h>
+
+// DEFINES
+/* #define NUM_THREADS 5 */
+
+// STRUCTURES
+/* typedef enum s_bool
+{
+	false,
+	true,
+}	t_bool; */
+
+/*_____           _        _                         
+ |  __ \         | |      | |                        
+ | |__) | __ ___ | |_ ___ | |_ _   _ _ __   ___  ___ 
+ |  ___/ '__/ _ \| __/ _ \| __| | | | '_ \ / _ \/ __|
+ | |   | | | (_) | || (_) | |_| |_| | |_) |  __/\__ \
+ |_|   |_|  \___/ \__\___/ \__|\__, | .__/ \___||___/
+                                __/ | |              
+                               |___/|_|            */
+
+//////////////////////////////////////////////////////
+//						MAIN.C						//
+//////////////////////////////////////////////////////
+int		main(void);
