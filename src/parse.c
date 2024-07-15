880def3867778822b8bb86fc066c7cbb3cb8e0df
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-15 11:01:47 +0200

    Added decission tree to define functions to analize the input

--- src/parse.c
+++ src/parse.c
@@ -6,16 +6,29 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:15:16 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/12 14:48:32 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/07/15 10:46:38 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+int	analize_parts(char *input)
+{
+	analize_simple_quotes();
+	analize_double_quotes();
+	analize_infile();
+	analize_outfile();
+	analize_built_in();
+}
+
 int	parse(char *input)
 {
-	char **split;
+	char	**split;
+	t_mini	mini;
 
+	ft_bzero(&mini, sizeof(t_mini));
+	analize_parts(input);
+	//input = analize_comillas(input);
 	printf("You entered: %s\n", input);
 	split = ft_split(input, '|');
 	int i = 0;
@@ -24,7 +37,6 @@ int	parse(char *input)
 		printf("split[%d] = %s\n", i, split[i]);
 		i++;
 	}
-
 	return (0);
 }
 /*
