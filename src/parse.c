926c7ced0f6641a276b38a8588cd0ff8e302d90a
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-15 13:42:53 +0200

    Added ft_tokenize() call (even whit bugs) for testing

--- src/parse.c
+++ src/parse.c
@@ -6,35 +6,36 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:15:16 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/15 10:46:38 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/07/15 13:34:19 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	analize_parts(char *input)
+/* int	analize_parts(char *input)
 {
 	analize_simple_quotes();
 	analize_double_quotes();
 	analize_infile();
 	analize_outfile();
 	analize_built_in();
-}
+} */
 
 int	parse(char *input)
 {
-	char	**split;
+	char	**tokens;
 	t_mini	mini;
 
-	ft_bzero(&mini, sizeof(t_mini));
-	analize_parts(input);
-	//input = analize_comillas(input);
 	printf("You entered: %s\n", input);
-	split = ft_split(input, '|');
+	ft_bzero(&mini, sizeof(t_mini));
+	tokens = ft_tokenize(input, ' ');
+	//analize_parts(input);
+	//input = analize_comillas(input);
+	//split = ft_split(input, '|');
 	int i = 0;
-	while (split[i])
+	while (tokens[i])
 	{
-		printf("split[%d] = %s\n", i, split[i]);
+		printf("tokens[%d] = %s\n", i, tokens[i]);
 		i++;
 	}
 	return (0);
