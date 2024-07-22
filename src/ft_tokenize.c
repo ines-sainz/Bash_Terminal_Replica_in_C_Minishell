c661be6e18be8bfb41e7475743ed9755ba4c16c4
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-22 14:55:02 +0200

    Added new code of ft_tokenize()

--- src/ft_tokenize.c
+++ src/ft_tokenize.c
@@ -6,163 +6,57 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 12:49:02 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/07/17 10:42:42 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/07/22 14:54:12 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static int	ft_count_elements(const char *s, char c)
+static void	add_to_args(char *input, char *args[], int *argc, char **arg_ptr, char *arg)
 {
-	int		count;
-	int		i;
-	int		sim_quote;
-	int		dbl_quote;
-	int		flag;
+	int	in_single_quote;
+	int	in_double_quote;
 
-	count = 0;
-	i = 0;
-	sim_quote = 0;
-	dbl_quote = 0;
-	flag = 0;
-	while (s[i] != '\0')
+	in_single_quote = 0;
+	in_double_quote = 0;
+	while (*input)
 	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != c && s[i] != '\0')
-			count++;
-		if (s[i] == '\'')
-			sim_quote = 1;
-		else if (s[i] == '\"')
-			dbl_quote = 1;
-		while ((s[i] != c || sim_quote == 1 || dbl_quote == 1) && s[i] != '\0')
+		if (*input == '\'' && !in_double_quote)
+			in_single_quote = !in_single_quote;
+		else if (*input == '\"' && !in_single_quote)
+			in_double_quote = !in_double_quote;
+		else if (ft_isspace(*input) && !in_single_quote && !in_double_quote)
 		{
-			i++;
-			if ((s[i] == '\'' && sim_quote == 1 && s[i + 1] == c))
+			if (*arg_ptr != arg)
 			{
-				if (flag != 0)
-					flag--;
-				else
-					sim_quote = 0;
+				**arg_ptr = '\0';
+				args[(*argc)++] = ft_strdup(arg);
+				*arg_ptr = arg;
 			}
-			else if ((s[i] == '\'' && sim_quote == 1 && s[i + 1] != c))
-				flag++;
-			if ((s[i] == '\"' && dbl_quote == 1 && s[i + 1] == c))
-			{
-				if (flag != 0)
-					flag--;
-				else
-					dbl_quote = 0;
-			}
-			else if ((s[i] == '\"' && dbl_quote == 1 && s[i + 1] != c))
-				flag++;
 		}
-	}
-	printf("palabras = %i\n", count);
-	return (count);
-}
-
-static void	free_split_awk(char **split, int count)
-{
-	while (count > 0)
-	{
-		count--;
-		free(split[count]);
-	}
-}
-
-static int	find_string(int *i, char c, const char *s, int *flag)
-{
-	int	start;
-	int	sim_quote;
-	int	dbl_quote;
-
-	start = 0;
-	sim_quote = 0;
-	dbl_quote = 0;
-	while (s[*i] == c)
-		(*i)++;
-	if (s[*i] == '\0')
-		return (start);
-	start = *i;
-	if (s[*i] == '\'')
-		sim_quote = 1;
-	else if (s[*i] == '\"')
-		dbl_quote = 1;
-	while ((s[*i] != c || sim_quote == 1 || dbl_quote == 1) && s[*i] != '\0')
-	{
-		(*i)++;
-		if ((s[*i] == '\'' && sim_quote == 1 && s[*i + 1] == c))
-		{
-			if (*flag != 0)
-				(*flag)--;
-			else
-				sim_quote = 0;
-		}
-		else if ((s[*i] == '\'' && sim_quote == 1 && s[*i + 1] != c))
-			(*flag)++;
-		if ((s[*i] == '\"' && dbl_quote == 1 && s[*i + 1] == c))
-		{
-			if (*flag != 0)
-				(*flag)--;
-			else
-				dbl_quote = 0;
-		}
-		else if ((s[*i] == '\"' && dbl_quote == 1 && s[*i + 1] != c))
-			(*flag)++;
-	}
-	/* if (sim_quote == 1 || dbl_quote == 1)
-		(*i)++; */
-	return (start);
-}
-
-static char	**ft_copy_string(const char *s, char c, char **split)
-{
-	int	start;
-	int	count;
-	int	i;
-	int	flag;
-
-	start = 0;
-	count = 0;
-	i = 0;
-	flag = 0;
-	while (s[i])
-	{
-		start = find_string(&i, c, s, &flag);
-		printf("flag == %i\n", flag);
-		if (s[start] == '\'' || s[start] == '\"')
-			start++;
-		split[count] = (char *)malloc((i - start - flag) * sizeof(char));
-		if (!split[count])
-		{
-			free_split_awk(split, count);
-			return (NULL);
-		}
-		if (s[start] == '\'' || s[start] == '\"')
-			ft_strlcpy(split[count++], s + start, i - start - flag);
 		else
-			ft_strlcpy(split[count++], s + start, i - start + 1  - flag);
+			*(*arg_ptr)++ = *input;
+		input++;
 	}
-	split[count] = NULL;
-	return (split);
 }
 
-char	**ft_tokenize(char const *s, char c)
+void	ft_tokenize(char *input, char *args[], int *argc)
 {
-	char	**split;
-	int		count;
+	int		in_single_quote;
+	int		in_double_quote;
+	char	*arg;
+	char	*arg_ptr;
 
-	if (s == NULL)
-		return (NULL);
-	count = ft_count_elements(s, c);
-	split = (char **)malloc((count + 1) * sizeof(char *));
-	if (!split)
-		return (NULL);
-	if (!ft_copy_string(s, c, split))
+	in_single_quote = 0;
+	in_double_quote = 0;
+	arg = malloc(ft_strlen(input) + 1);
+	arg_ptr = arg;
+	*argc = 0;
+	add_to_args(input, args, argc, &arg_ptr, arg);
+	if (arg_ptr != arg)
 	{
-		free(split);
-		return (NULL);
+		*arg_ptr = '\0';
+		args[(*argc)++] = ft_strdup(arg);
 	}
-	return (split);
+	free(arg);
 }
