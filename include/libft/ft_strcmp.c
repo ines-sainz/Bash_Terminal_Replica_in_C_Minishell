b8fe119a21dad07a79623b5e2dff9c94b8059640
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-23 13:02:27 +0200

    Now it works with index

--- include/libft/ft_strcmp.c
+++ include/libft/ft_strcmp.c
@@ -14,14 +14,16 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
+	int	i;
+
+	i = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (0);
-	while (*s1 != '\0' || *s2 != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
