124ce174c673f8c3b37dba09c922d00270b005f6
BishopVK <danielgraindorge@gmail.com>
2024-08-16 20:46:32 +0200

    Separeted code from add_to_args() in diferent functions

--- src/ft_tokenize.c
+++ src/ft_tokenize.c
@@ -6,91 +6,87 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 12:49:02 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/08/16 18:54:27 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/08/16 20:39:20 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-/* static void	control_quotes(char	*input_ptr, t_bool	in_single_quote, t_bool	in_double_quote)
+static void	dollar_out_of_single_quotes(char *input_ptr, t_args *args)
 {
-	if (*input_ptr == '\'' && !in_double_quote)
-		in_single_quote = !in_single_quote;
-	else if (*input_ptr == '\"' && !in_single_quote)
-		in_double_quote = !in_double_quote;
-} */
-
-static void	add_to_args(t_args *args, int *argc, char **arg_ptr)
-{
-	char	*input_ptr;
-	char	*expanded_arg;
 	char	*next_char;
-
-	input_ptr = args->input;
-	while (*input_ptr)
+	
+	next_char = input_ptr + 1;
+	if (ft_isdigit(*next_char) || *next_char == '*')
 	{
-		if (*input_ptr == '\'' && !args->in_double_quote)
-			args->in_single_quote = !args->in_single_quote;
-		else if (*input_ptr == '\"' && !args->in_single_quote)
-			args->in_double_quote = !args->in_double_quote;
-		if ((ft_isspace(*input_ptr) || *input_ptr == '|') && !args->in_single_quote && !args->in_double_quote)
-		{
-			if (*arg_ptr != args->arg)
-			{
-				**arg_ptr = '\0';
-				expanded_arg = expander(args->arg, args->in_single_quote);
-				if (expanded_arg)
-					args->args[(*argc)++] = expanded_arg;
-				*arg_ptr = args->arg;
-			}
-			if (*input_ptr == '|')
-				args->args[(*argc)++] = ft_strdup("|");
-		}
-		else if (*input_ptr == '$' && !args->in_single_quote)
-		{
-			next_char = input_ptr + 1;
-			if (isdigit(*next_char) || *next_char == '*')
-			{
-				next_char++;
-				input_ptr = next_char;
-				continue;
-			}
-			else
-				*(*arg_ptr)++ = *input_ptr;
-		}
-		else
-			*(*arg_ptr)++ = *input_ptr;
-		input_ptr++;
+		next_char++;
+		input_ptr = next_char;
 	}
+	else
+		*args->arg_ptr++ = *input_ptr;
+}
 
-	// Aquí verificamos si hay comillas no cerradas
+static void	out_of_quotes(char *input_ptr, t_args *args, int **argc)
+{
+	char	*expanded_arg;
+
+	if (args->arg_ptr != args->arg)
+	{
+		*args->arg_ptr = '\0';
+		expanded_arg = expander(args->arg, args->in_single_quote);
+		if (expanded_arg)
+			args->args[(**argc)++] = expanded_arg;
+		args->arg_ptr = args->arg;
+	}
+	if (*input_ptr == '|')
+		args->args[(**argc)++] = ft_strdup("|");
+}
+
+static void	control_quotes(char *input_ptr, t_args *args)
+{
+	if (*input_ptr == '\'' && !args->in_double_quote)
+		args->in_single_quote = !args->in_single_quote;
+	else if (*input_ptr == '\"' && !args->in_single_quote)
+		args->in_double_quote = !args->in_double_quote;
+}
+
+static void	verify_closed_quotes(t_args *args)
+{
 	if (args->in_single_quote)
 	{
-		*(*arg_ptr)++ = '\''; // Cierra la comilla simple
+		*args->arg_ptr++ = '\''; // Cierra la comilla simple
 		args->in_single_quote = false;
 	}
 	if (args->in_double_quote)
 	{
-		*(*arg_ptr)++ = '\"'; // Cierra la comilla doble
+		*args->arg_ptr++ = '\"'; // Cierra la comilla doble
 		args->in_double_quote = false;
 	}
+}
 
-	if (*arg_ptr != args->arg)
+void	add_to_args(t_args *args, int *argc)
+{
+	char	*input_ptr;
+	char	*expanded_arg;
+
+	input_ptr = args->input;
+	while (*input_ptr)
 	{
-		**arg_ptr = '\0';
+		control_quotes(input_ptr, args);
+		if ((ft_isspace(*input_ptr) || *input_ptr == '|') && !args->in_single_quote && !args->in_double_quote)
+			out_of_quotes(input_ptr, args, &argc);
+		else if (*input_ptr == '$' && !args->in_single_quote)
+			dollar_out_of_single_quotes(input_ptr, args);
+		else
+			*args->arg_ptr++ = *input_ptr;
+		input_ptr++;
+	}
+	verify_closed_quotes(args);
+	if (args->arg_ptr != args->arg)
+	{
+		*args->arg_ptr = '\0';
 		expanded_arg = expander(args->arg, args->in_single_quote);
 		if (expanded_arg)
 			args->args[(*argc)++] = expanded_arg;
 	}
-}
-
-void ft_tokenize(t_args *args, int *argc)
-{
-	args->arg = malloc(ft_strlen(args->input) + 1);
-	args->arg_ptr = args->arg;
-	*argc = 0;
-	args->in_single_quote = false;
-	args->in_double_quote = false;
-	add_to_args(args, argc, &args->arg_ptr);
-	free(args->arg);
 }
