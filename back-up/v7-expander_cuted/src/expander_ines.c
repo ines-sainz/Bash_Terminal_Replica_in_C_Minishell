aff2340b203ff430008126eca2416a734ab085ae
BishopVK <danielgraindorge@gmail.com>
2024-08-23 20:22:36 +0200

    Created back-up whit expander() cuted in functions whit less than 25 lines

---
+++ back-up/v7-expander_cuted/src/expander_ines.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expander_ines.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 09:07:57 by isainz-r          #+#    #+#             */
+/*   Updated: 2024/08/20 12:19:11 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+//Sólo entra aquí el dolar y la palabra de después $....
+char	*expander(char *variable)
+{
+	char	*expanded;
+
+	if (variable[1] >= '0' && variable[1] <= '9')
+		return (ft_substr(variable, 2, (ft_strlen(variable) - 2)));
+	expanded = getenv(++variable);
+	if (!expanded)
+		return (ft_strdup("\n"));
+	return (expanded);
+}
