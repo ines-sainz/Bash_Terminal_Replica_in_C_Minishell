81eb82c2f20ccc17ac23a04f89e79876a04a6c13
BishopVK <danielgraindorge@gmail.com>
2024-08-27 22:36:03 +0200

    Added del_params() function to delete list of params. Added call to add_arguments_to_list() to create a list whith the arguments at args->args. Added while to print the list for testing

--- src/parse.c
+++ src/parse.c
@@ -6,13 +6,33 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:15:16 by danjimen          #+#    #+#             */
-/*   Updated: 2024/08/23 21:25:26 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/08/27 20:34:09 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 #define MAX_ARGS 100
+
+void	del_params(t_args *args)
+{
+	t_params	*current;
+	t_params	*next;
+
+	current = args->params;
+	if (args->params != NULL)
+	{
+		while (current != NULL)
+		{
+			next = current->next;
+			if (current->content != NULL)
+				free(current->content);
+			free(current);
+			current = next;
+		}
+		args->params = NULL;
+	}
+}
 
 static void	ft_tokenize(t_args *args, int *argc, t_mini *mini)
 {
@@ -27,9 +47,9 @@ static void	ft_tokenize(t_args *args, int *argc, t_mini *mini)
 
 int	parse(t_args *args, t_mini *mini)
 {
-	int	i;
+	int			i;
+	t_params	*temp;
 
-	//(void)mini;
 	printf("You entered: %s\n", args->input);
 	ft_tokenize(args, &args->argc, mini);
 	printf("argc ==> %i\n", args->argc);
@@ -42,9 +62,16 @@ int	parse(t_args *args, t_mini *mini)
 		if (args->args[i])
 		{
 			printf("arg[%d]: %s\n", i, args->args[i]);
+			add_argument_to_list(args, &i);
 			free(args->args[i]);
 		}
 		i++;
+	}
+	temp = args->params;
+	while (temp != NULL)
+	{
+		printf("arg[%d]: %s, type: %d\n", temp->argc, temp->content, temp->type);
+		temp = temp->next;
 	}
 	return (0);
 }
