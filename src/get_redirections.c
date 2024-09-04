1915bf9d4b51bdc05fc971bf45d29464cfa8b94a
Ines Sainz Ramon <isainz-r@c1r11s1.42madrid.com>
2024-09-04 17:45:30 +0200

    to find the redirections in the commands

--- src/get_redirections.c
+++ src/get_redirections.c
@@ -12,7 +12,23 @@
 
 #include "../include/minishell.h"
 
-/*void	get_redirections(t_args *args, t_mini *mini)
+/*void	redirect(t_params *iter_params, int n_command, t_args *iter_pipes,
+	t_mini *mini)
+{
+	while (iter_params != NULL && iter_params != PIPE)
+	{
+		if (iter_params->type == INFILE)
+			infile(iter_params->next, iter_pipes, mini);
+		if (iter_params->type == HERE_DOC)
+			here_doc(iter_params->next, iter_pipes, mini);
+		if (iter_params->type == APPEND)
+			append(iter_params->next, iter_pipes, mini);
+		if (iter_params->type == OUTFILE)
+			outfile(iter_params->next, iter_pipes, mini);
+	}
+}*/
+
+void	get_redirections(t_args *args, t_mini *mini)
 {
 	t_params	*iter_params;
 	t_pipes		*iter_pipes;
@@ -21,21 +37,12 @@
 	iter_params = args->params;
 	iter_pipes = mini->first_pipe;
 	n_commands = 1;
-	if (iter_params == INFILE || iter_params == OUTFILE
-		|| iter_params == APPEND)
-	{
-		redirections(iter_params, mini);
-		iter_params = iter_params->next;
-	}
 	while (iter_params != NULL)
 	{
-		while (iter_params->type != PIPE)
-		{
-			if (iter_params == INFILE || iter_params == OUTFILE ||
-				iter_params == APPEND iter_params->next = )
+		//redirect(iter_params, );
+		while (iter_params != PIPE || iter_params->next != NULL)
 			iter_params = iter_params->next;
-		}
-		n_commands++;
+		iter_pipes = iter_pipes->next;
 		iter_params = iter_params->next;
 	}
-}*/
+}
