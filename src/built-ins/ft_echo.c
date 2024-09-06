b824541f8189da532ffe5fdeeb36a06de13682bd
BishopVK <danielgraindorge@gmail.com>
2024-09-06 22:31:41 +0200

    Refactorized

--- src/built-ins/ft_echo.c
+++ src/built-ins/ft_echo.c
@@ -3,58 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:28:19 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/09/05 14:37:31 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/09/06 22:31:11 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ft_built_echo(t_args *args)
+static void	detect_and_count_flag(t_args *args, int *i, int	*n_flag)
 {
-	int	i;
-	int	j;
 	int	flag;
-	int	argc;
-	int	n_flag;
+	int	j;
 
 	flag = 0;
-	argc = 0;
-	n_flag = 0;
-	i = 0;
-	while (args->args[i++])
-		argc++;
-	printf("echo argc = %i\n", argc);
-	if (argc == 1)
+	while (args->args[*i] && ft_strlen(args->args[*i]) >= 2)
 	{
-		printf("echo devuelve:\n");
-		printf("\n");
-		return ;
-	}
-	i = 1;
-	while (args->args[i] && ft_strlen(args->args[i]) >= 2)
-	{
-		if (args->args[i][0] != '-')
+		if (args->args[*i][0] != '-')
 			break ;
 		else
 		{
 			j = 1;
-			while (args->args[i][j])
+			while (args->args[*i][j])
 			{
-				if (args->args[i][j] != 'n')
+				if (args->args[*i][j] != 'n')
 					flag++;
 				j++;
 			}
 			if (flag == 0)
-				n_flag++;
+				(*n_flag)++;
 			else
 				break ;
 		}
-		i++;
+		(*i)++;
 	}
-	printf("echo devuelve: ");
+}
+
+void	ft_built_echo(t_args *args)
+{
+	int	i;
+	int	argc;
+	int	n_flag;
+
+	argc = 0;
+	while (args->args[argc])
+		argc++;
+	if (argc == 1)
+	{
+		printf("echo devuelve:\n"); //Eliminar
+		printf("\n");
+		return ;
+	}
+	i = 1;
+	n_flag = 0;
+	detect_and_count_flag(args, &i, &n_flag);
+	printf("echo devuelve: "); //Eliminar
 	while (args->args[i])
 	{
 		printf("%s", args->args[i]);
