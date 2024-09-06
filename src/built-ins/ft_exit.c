a8ca31cf8f6029f56e7a2b712256a7a32cedb756
BishopVK <danielgraindorge@gmail.com>
2024-09-06 19:43:47 +0200

    Finished exit built-in

--- src/built-ins/ft_exit.c
+++ src/built-ins/ft_exit.c
@@ -3,23 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:40:06 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/09/06 15:03:00 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/09/06 19:40:28 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ft_built_exit(t_args *args)
+static int	its_only_numbers(char *str)
 {
 	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] == '-')
+			i++;
+		if (!ft_isdigit(str[i]))
+			return (ERR);
+		i++;
+	}
+	return (OK);
+}
+
+static void	multiple_args(t_args *args, int argc, t_mini *mini)
+{
+	int	nbr_atoi;
+	int	modulus;
+
+	if (its_only_numbers(args->args[1]) == ERR)
+	{
+		free_at_exit(args);
+		exit(2);
+	}
+	else if (its_only_numbers(args->args[1]) == OK && argc == 2)
+	{
+		nbr_atoi = ft_atoi(args->args[1]);
+		modulus = (nbr_atoi % 256);
+		free_at_exit(args);
+		exit(modulus);
+	}
+	else if (its_only_numbers(args->args[1]) == OK && argc > 2)
+		ft_export_env("?=1", mini);
+}
+
+void	ft_built_exit(t_args *args, t_mini *mini)
+{
 	int	argc;
 
 	argc = 0;
-	i = 0;
-	while (args->args[i++])
+	while (args->args[argc])
 		argc++;
 	printf("exit argc = %i\n", argc);
 	if (argc == 1)
@@ -28,18 +63,5 @@ void	ft_built_exit(t_args *args)
 		exit(0);
 	}
 	if (argc >= 2)
-	{
-		i = 0;
-		while (args->args[1][i])
-		{
-			if (ft_isdigit(args->args[1][i]) == 0) // Not Digit detected
-			{
-				free_at_exit(args);
-				exit(2);
-			}
-			i++;
-		}
-	}
-	// free_at_exit(args);
-	// exit(0);
+		multiple_args(args, argc, mini);
 }
