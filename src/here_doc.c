c6b347f54801e60c6ddcafa08dfcd43b2e94bdae
Ines Sainz Ramon <isainz-r@c1r11s2.42madrid.com>
2024-09-11 15:38:15 +0200

    here_doc worksgit add .git add . no here_doc expansions

--- src/here_doc.c
+++ src/here_doc.c
@@ -23,10 +23,11 @@ char	*ft_get_eof(char *eof)
 	return (eof_fin);
 }
 
-int	ft_write_temp(int fd, char *eof, char *buffer)
+int	ft_write_temp(int fd, char *eof, char *buffer, t_mini *mini)
 {
 	int	n_bytes;
 
+	(void)mini;
 	while (1)
 	{
 		write(1, ">", 1);
@@ -41,29 +42,54 @@ int	ft_write_temp(int fd, char *eof, char *buffer)
 		buffer[n_bytes] = '\0';
 		if (!ft_strncmp(ft_get_eof(eof), buffer, n_bytes))
 			break ;
-		write(fd, buffer, n_bytes);
+		//aqui hay que expandir despues de comparar
+		write(fd, buffer, n_bytes); //aqui hay que cambiaar n_bytes por el len de la expansion
 	}
 	return (0);
 }
 
-int	ft_here_doc(int i, t_params *param)
+char	*get_here_doc_file_name()
 {
-	char	buffer[1024];
-	int		fd;
+	int		i;
 	char	*file_num;
 	char	*file_name;
 
+	i = 0;
 	file_num = ft_itoa(i);
-	file_name = ft_strjoin(file_num, ".txt");
+	file_name = ft_strjoin(file_num, ".tmp");
 	free(file_num);
+	while (access(file_name, F_OK) == 0)
+	{
+		i++;
+		free(file_name);
+		file_num = ft_itoa(i);
+		file_name = ft_strjoin(file_num, ".tmp");
+		free(file_num);
+	}
+	return (file_name);
+}
+
+int	ft_here_doc(t_params *param, t_mini *mini, int fd)
+{
+	char	buffer[1024];
+	t_list	*temp_here_doc;
+	char	*file_name;
+
+	file_name = get_here_doc_file_name();
 	fd = open(file_name, O_RDWR | O_CREAT | O_TRUNC, 0777);
 	if (fd < 0)
+		return (free(file_name), ERR);
+	temp_here_doc = ft_lstnew(file_name);
+	if (temp_here_doc == NULL)
+	{
+		ft_lstclear(&mini->here_doc_files, free);
 		return (ERR);
-	if (ft_write_temp(fd, param->content, buffer) == 1)
+	}
+	ft_lstadd_back(&mini->here_doc_files, temp_here_doc);
+	if (ft_write_temp(fd, param->content, buffer, mini) == 1)
 		return (ERR);
 	close (fd);
 	fd = open(file_name, O_RDONLY, 0777);
-	free(file_name);
 	return (fd);
 }
 
@@ -71,8 +97,8 @@ int	*get_here_doc(t_params *iter_params, t_args *args)
 {
 	int	i;
 	int	*here_doc_fds;
+	int	fd;
 
-	write(1, "a\n", 2);
 	i = 0;
 	while (iter_params != NULL)
 	{
@@ -80,14 +106,15 @@ int	*get_here_doc(t_params *iter_params, t_args *args)
 			i++;
 		iter_params = iter_params->next;
 	}
-	here_doc_fds = malloc(i + 1 * sizeof(int *));
+	here_doc_fds = malloc(i * sizeof(int *));
 	i = 0;
 	iter_params = args->params;
 	while (iter_params != NULL)
 	{
 		if (iter_params->type == HERE_DOC)
 		{
-			here_doc_fds[i] = ft_here_doc(i, iter_params->next);
+			fd = 0;
+			here_doc_fds[i] = ft_here_doc(iter_params->next, args->mini, fd);
 			i++;
 		}
 		iter_params = iter_params->next;
@@ -97,7 +124,6 @@ int	*get_here_doc(t_params *iter_params, t_args *args)
 
 void	set_here_doc(int fd, t_pipes *iter_pipes, t_mini *mini)
 {
-	//////////// Inés, he añadido esto para poder compilar
 	//primero abre here_doc luego escribe que se ha roto
 	if (iter_pipes->inf_pipe < 0 || iter_pipes->outf_pipe < 0)
 	{
@@ -109,8 +135,6 @@ void	set_here_doc(int fd, t_pipes *iter_pipes, t_mini *mini)
 	iter_pipes->inf_pipe = fd;
 	if (iter_pipes->inf_pipe < 0)
 	{
-		//////////// Inés, he comentado esto para poder compilar
-		//printf("minishell: Error opening here_doc\n", iter_params->content);
 		printf("minishell: Error opening here_doc\n");
 		ft_export_env("?=1", mini);
 	}
