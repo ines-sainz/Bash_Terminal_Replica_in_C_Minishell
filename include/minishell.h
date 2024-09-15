59b5dc92879b66ffa7d36fc84855e1cefcc0c5b2
BishopVK <danielgraindorge@gmail.com>
2024-09-15 20:23:47 +0200

    Added signal_sigint() prototype in main.c file

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 07:52:36 by danjimen          #+#    #+#             */
-/*   Updated: 2024/09/14 21:19:42 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/09/15 20:23:17 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,6 +188,7 @@ typedef struct s_args
 //////////////////////////////////////////////////////
 //						MAIN.C						//
 //////////////////////////////////////////////////////
+void		signal_sigint(int sig);
 void		free_at_exit(t_args *args);
 //int		main(void);
 
