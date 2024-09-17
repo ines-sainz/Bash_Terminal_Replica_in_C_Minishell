ad2346a90da04bf2035119cb63d0f2311d84c178
Ines Sainz Ramon <isainz-r@c1r11s2.42madrid.com>
2024-09-17 16:33:07 +0200

    redirections and execution splitted in files

---
+++ src/execution_structure.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution_structure.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isainz-r <isainz-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 14:39:43 by isainz-r          #+#    #+#             */
+/*   Updated: 2024/09/16 14:39:45 by isainz-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+t_execution	*exe_struct_new(int i)
+{
+	t_execution	*node;
+
+	node = (t_execution *)malloc(sizeof(t_execution));
+	if (!node)
+		return (NULL);
+	node->n_command = i;
+	node->command = NULL;
+	node->type = PARAMS;
+	node->inf_pipe = 0;
+	node->outf_pipe = 1;
+	node->next = NULL;
+	return (node);
+}
+
+void	exe_struct_free(t_mini *mini)
+{
+	t_execution	*next;
+	int			i;
+
+	while (mini->exe_command != NULL)
+	{
+		next = mini->exe_command->next;
+		i = 0;
+		if (mini->exe_command->command)
+		{
+			while (mini->exe_command->command[i])
+			{
+				free(mini->exe_command->command[i]);
+				i++;
+			}
+			free(mini->exe_command->command);
+			mini->exe_command->command = NULL;
+		}
+		free(mini->exe_command);
+		mini->exe_command = next;
+	}
+	mini->exe_command = NULL;
+}
+
+void	exe_struct_add_back(t_execution *node, t_mini *mini)
+{
+	t_execution	*iter;
+
+	if (!mini->exe_command)
+	{
+		mini->exe_command = node;
+		return ;
+	}
+	iter = mini->exe_command;
+	while (iter->next != NULL)
+		iter = iter->next;
+	iter->next = node;
+}
+
+int	create_execution_struct(t_mini *mini)
+{
+	int			i;
+	t_execution	*node;
+
+	i = 1;
+	mini->exe_command = NULL;
+	while (i <= mini->n_commands)
+	{
+		node = exe_struct_new(i);
+		if (!node)
+		{
+			exe_struct_free(mini);
+			return (ERR);
+		}
+		exe_struct_add_back(node, mini);
+		i++;
+	}
+	return (0);
+}
