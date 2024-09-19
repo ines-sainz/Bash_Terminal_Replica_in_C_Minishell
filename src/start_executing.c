6269b803be96bdfd4aa0d1054f5906a82adda9a2
Ines Sainz Ramon <isainz-r@c1r11s2.42madrid.com>
2024-09-19 11:00:33 +0200

    add execution. The exit not work and here_doc with space as eof

---
+++ src/start_executing.c
@@ -0,0 +1,178 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   start_executing.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isainz-r <isainz-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 12:13:15 by isainz-r          #+#    #+#             */
+/*   Updated: 2024/09/18 12:13:16 by isainz-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+char	*check_command(char **path_list, char *path_mid, char *kid)
+{
+	int		j;
+	int		i;
+	char	*path_fin;
+
+	j = -1;
+	while (path_list[++j])
+	{
+		path_mid = ft_strjoin(path_list[j], "/");
+		path_fin = ft_strjoin(path_mid, kid);
+		free(path_mid);
+		if (access(path_fin, X_OK) == 0)
+		{
+			i = 0;
+			while (path_list[i])
+			{
+				free(path_list[i]);
+				i++;
+			}
+			free(path_list);
+			return (path_fin);
+		}
+		free(path_fin);
+	}
+	return (NULL);
+}
+
+char	*get_path_command(char **kid, char **env, char *path_mid)
+{
+	int		i;
+	char	*path;
+	char	**path_list;
+
+	if (ft_strchr(kid[0], '/'))
+		if (access(kid[0], X_OK) == 0)
+			return (ft_strdup(kid[0]));
+	i = -1;
+	while (env[++i])
+	{
+		if (!ft_strncmp("PATH=", env[i], 5))
+		{
+			path_list = ft_split(env[i], ':');
+			path = check_command(path_list, path_mid, kid[0]);
+			if (path)
+				return (path);
+		}
+	}
+	i = 0;
+	while (path_list[i])
+	{
+		free(path_list[i]);
+		i++;
+	}
+	return (free(path_list), NULL);
+}
+
+int	execute(t_execution *iter_exe, t_mini *mini)
+{
+	char	*path_mid;
+	char	*path_command;
+	int		i;
+
+	if (iter_exe->inf_pipe != 0)
+	{
+		dup2(iter_exe->inf_pipe, 0);
+		close(iter_exe->inf_pipe);
+	}
+	if (iter_exe->outf_pipe != 1)
+	{
+		dup2(iter_exe->outf_pipe, 1);
+		close(iter_exe->outf_pipe);
+	}
+	path_mid = NULL;
+	path_command = get_path_command(iter_exe->command, mini->env, path_mid);
+	if (path_command)
+	{
+		printf("execve-> path_command: %s\n", path_command);
+	}
+	i = 0;
+	while (iter_exe->command[i])
+	{
+		printf("execve com: %s\n", iter_exe->command[i]);
+		i++;
+	}
+	i = 0;
+	execve(path_command, iter_exe->command, mini->env);
+	//returns y frees
+	return (127);
+}
+
+int	check_built_ins(char **command, t_mini *mini, t_args *args)
+{
+	int	len;
+
+	len = ft_strlen(command[0]);
+	if (ft_strncmp(command[0], "echo", len) == 0 && len == 4)
+	{
+		ft_built_echo(command);
+		return (1);
+	}
+	if (ft_strncmp(command[0], "cd", len) == 0 && len == 2)
+	{
+		ft_built_cd(command, mini);
+		return (1);
+	}
+	if (ft_strncmp(command[0], "pwd", len) == 0 && len == 3)
+	{
+		ft_built_pwd(command, mini);
+		return (1);
+	}
+	if (ft_strncmp(command[0], "export", len) == 0 && len == 6)
+	{
+		ft_built_export(command, mini);
+		return(1);
+	}
+	if (ft_strncmp(command[0], "unset", len) == 0 && len == 5)
+	{
+		ft_built_unset(command, mini);
+		return (1);
+	}
+	if (ft_strncmp(command[0], "env", len) == 0 && len == 3)
+	{
+		ft_print_env(mini);
+		return (1);
+	}
+	if (ft_strncmp(command[0], "exit", len) == 0 && len == 4)
+	{
+		ft_built_exit(args, mini);
+		return (1);
+	}
+	return (0);
+}
+
+int	start_executing(t_execution *iter_exe, t_mini *mini, t_args *args)
+{
+	pid_t	pid;
+	int		status;
+
+	while (iter_exe != NULL)
+	{
+		if (mini->n_commands == 1)
+		{
+			if (check_built_ins(iter_exe->command, mini, args) == 1)
+				return (0);
+		}
+		pid = fork();
+		printf("pid: %i\n", pid);
+		if (pid == 0)
+		{
+			if (check_built_ins(iter_exe->command, mini, args) == 0)
+				execute(iter_exe, mini);
+			else
+			{
+				printf("sali del built_in\n");
+				exit (0);
+			}
+		}
+		iter_exe = iter_exe->next;
+	}
+	while (waitpid(-1, &status, 0) != -1)
+		continue ;
+	return (WEXITSTATUS(status));
+}
