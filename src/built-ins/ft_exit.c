bbf660b031556ff521c89381131e562a018706ee
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-09-20 11:29:07 +0200

    Updates ft_built_exit prototype()

--- src/built-ins/ft_exit.c
+++ src/built-ins/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:40:06 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/09/07 18:54:03 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/09/20 11:15:00 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,35 +28,35 @@ static int	its_only_numbers(char *str)
 	return (OK);
 }
 
-static void	multiple_args(t_args *args, int argc, t_mini *mini)
+static void	multiple_args(t_args *args, char **exit_args, int argc, t_mini *mini)
 {
 	int	nbr_atoi;
 	int	modulus;
 
-	if (its_only_numbers(args->args[1]) == ERR)
+	if (its_only_numbers(exit_args[1]) == ERR)
 	{
 		printf("minishell: exit: %s: numeric argument required\n",
-			args->args[1]);
+			exit_args[1]);
 		free_at_exit(args);
 		exit(2);
 	}
-	else if (its_only_numbers(args->args[1]) == OK && argc == 2)
+	else if (its_only_numbers(exit_args[1]) == OK && argc == 2)
 	{
-		nbr_atoi = ft_atoi(args->args[1]);
+		nbr_atoi = ft_atoi(exit_args[1]);
 		modulus = (nbr_atoi % 256);
 		free_at_exit(args);
 		exit(modulus);
 	}
-	else if (its_only_numbers(args->args[1]) == OK && argc > 2)
+	else if (its_only_numbers(exit_args[1]) == OK && argc > 2)
 		ft_export_env("?=1", mini);
 }
 
-void	ft_built_exit(t_args *args, t_mini *mini)
+void	ft_built_exit(t_args *args, char **exit_args, t_mini *mini)
 {
 	int	argc;
 
 	argc = 0;
-	while (args->args[argc])
+	while (exit_args[argc])
 		argc++;
 	printf("exit argc = %i\n", argc);
 	if (argc == 1)
@@ -65,5 +65,5 @@ void	ft_built_exit(t_args *args, t_mini *mini)
 		exit(0);
 	}
 	if (argc >= 2)
-		multiple_args(args, argc, mini);
+		multiple_args(args, exit_args, argc, mini);
 }
