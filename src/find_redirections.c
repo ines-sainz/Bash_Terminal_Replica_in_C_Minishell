5fd494f8b8c70215068870bf8d67ca4529da8fe1
Ines Sainz Ramon <isainz-r@c2r2s4.42madrid.com>
2024-09-24 19:31:50 +0200

    DB: in debugging printfs and commented them

--- src/find_redirections.c
+++ src/find_redirections.c
@@ -18,11 +18,11 @@ void	fill_infile(t_params *iter_params, t_execution *iter_exe, t_mini *mini)
 		return ;
 	if (iter_exe->inf_pipe != 0)
 	{
-		printf("1 inf close: %i\n", iter_exe->inf_pipe);//
+		//printf("DB: 1 inf close: %i\n", iter_exe->inf_pipe);//
 		close (iter_exe->inf_pipe);
 	}
 	iter_exe->inf_pipe = open(iter_params->content, O_RDONLY);
-	printf("2 inf open: %i\n", iter_exe->inf_pipe);//
+	//printf("DB: 2 inf open: %i\n", iter_exe->inf_pipe);//
 	if (iter_exe->inf_pipe < 0)
 	{
 		printf("minishell: %s: No such file or directory\n",
@@ -35,17 +35,17 @@ void	fill_here_doc(int fd, t_execution *iter_exe, t_mini *mini)
 {
 	if (iter_exe->inf_pipe < 0 || iter_exe->outf_pipe < 0)
 	{
-		printf("3 here_doc close: %i\n", fd);//
+		//printf("DB: 3 here_doc close: %i\n", fd);//
 		close(fd);
 		return ;
 	}
 	if (iter_exe->inf_pipe != 0)
 	{
-		printf("4 inf close: %i\n", iter_exe->inf_pipe);//
+		//printf("DB: 4 inf close: %i\n", iter_exe->inf_pipe);//
 		close(iter_exe->inf_pipe);
 	}
 	iter_exe->inf_pipe = fd;
-	printf("5 here_doc set: %i\n", fd);//
+	//printf("DB: 5 here_doc set: %i\n", fd);//
 	if (iter_exe->inf_pipe < 0)
 	{
 		printf("minishell: Error opening here_doc\n");
@@ -59,12 +59,12 @@ void	fill_append(t_params *iter_params, t_execution *iter_exe, t_mini *mini)
 		return ;
 	if (iter_exe->outf_pipe != 1)
 	{
-		printf("6 outf close: %i\n", iter_exe->outf_pipe);//
+		//printf("DB: 6 outf close: %i\n", iter_exe->outf_pipe);//
 		close(iter_exe->outf_pipe);
 	}
 	iter_exe->outf_pipe = open(iter_params->content, O_WRONLY | O_CREAT
 		| O_APPEND, 0777);
-	printf("7 append open: %i\n", iter_exe->outf_pipe);//
+	//printf("DB: 7 append open: %i\n", iter_exe->outf_pipe);//
 	if (iter_exe->outf_pipe < 0)
 	{
 		printf("minishell: %s: Permission denied\n", iter_params->content);
@@ -78,12 +78,12 @@ void	fill_outfile(t_params *iter_params, t_execution *iter_exe, t_mini *mini)
 		return ;
 	if (iter_exe->outf_pipe != 1)
 	{
-		printf("8 outf close: %i\n", iter_exe->outf_pipe);
+		//printf("DB: 8 outf close: %i\n", iter_exe->outf_pipe);
 		close(iter_exe->outf_pipe);
 	}
 	iter_exe->outf_pipe = open(iter_params->content, O_RDWR | O_CREAT
 		| O_TRUNC, 0777);
-	printf("9 outf open: %i\n", iter_exe->outf_pipe);
+	//printf("DB: 9 outf open: %i\n", iter_exe->outf_pipe);
 	if (iter_exe->outf_pipe < 0)
 	{
 		printf("minishell: %s: Permission denied\n", iter_params->content);
@@ -96,28 +96,28 @@ void	fill_pipe(t_execution *iter_exe)
 	int	pipe_fds[2];
 
 	pipe(pipe_fds);
-	printf("10 inf %i, outf %i\n", pipe_fds[0], pipe_fds[1]);
+	//printf("DB: 10 inf %i, outf %i\n", pipe_fds[0], pipe_fds[1]);
 	if (iter_exe->outf_pipe == 1)
 	{
-		printf("11 pipe set outf: %i\n", pipe_fds[1]);
+		//printf("DB: 11 pipe set outf: %i\n", pipe_fds[1]);
 		iter_exe->outf_pipe = pipe_fds[1];
 	}
 	else if (iter_exe->outf_pipe != 1 && iter_exe->outf_pipe > 0)
 	{
-		printf("12 pipe outf close: %i\n", pipe_fds[1]);
+		//printf("DB: 12 pipe outf close: %i\n", pipe_fds[1]);
 		close(pipe_fds[1]);
-		printf("13 out_still: %i\n", iter_exe->outf_pipe);
+		//printf("DB: 13 out_still: %i\n", iter_exe->outf_pipe);
 	}
 	iter_exe = iter_exe->next;
 	if (iter_exe->inf_pipe == 0)
 	{
 		iter_exe->inf_pipe = pipe_fds[0];
-		printf("14 pipe set inf: %i\n", iter_exe->inf_pipe);
+		//printf("DB: 14 pipe set inf: %i\n", iter_exe->inf_pipe);
 	}
 	else if (iter_exe->inf_pipe > 0)
 	{
-		printf("15 inf close: %i\n", pipe_fds[0]);
+		//printf("DB: 15 inf close: %i\n", pipe_fds[0]);
 		close(pipe_fds[0]);
-		printf("16 inf_still: %i\n", iter_exe->inf_pipe);
+		//printf("DB: 16 inf_still: %i\n", iter_exe->inf_pipe);
 	}
 }
