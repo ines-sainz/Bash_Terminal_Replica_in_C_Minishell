ac7783579a4b6240b35deb7a48ceda18ee1646f8
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-09-24 11:34:18 +0200

    Norminette fixed

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,27 +6,11 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:15:16 by danjimen          #+#    #+#             */
-/*   Updated: 2024/09/18 14:42:49 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/09/24 11:33:55 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-/* static void	switch_to_delimiter(t_args *args)
-{
-	t_params	*temp;
-
-	temp = args->params;
-	while (temp != NULL)
-	{
-		if (temp->type == HERE_DOC && temp->next)
-		{
-			temp->next->type = DELIMITER;
-			temp = temp->next;
-		}
-		temp = temp->next;
-	}
-} */
 
 int	ft_tokenize(t_args *args, t_mini *mini)
 {
@@ -46,85 +30,28 @@ int	ft_tokenize(t_args *args, t_mini *mini)
 	return (OK);
 }
 
-/*void	close_fds(t_mini *mini)
-{
-	t_pipes	*pipe_iter;
-	t_list	*temp_here_doc;
-
-	pipe_iter = mini->first_pipe;
-	while (pipe_iter != NULL)
-	{
-		if (pipe_iter->inf_pipe > 0)
-			close(pipe_iter->inf_pipe);
-		if (pipe_iter->outf_pipe > 1)
-			close(pipe_iter->outf_pipe);
-		pipe_iter = pipe_iter->next;
-	}
-	temp_here_doc = mini->here_doc_files;
-	while (temp_here_doc != NULL)
-	{
-		unlink(temp_here_doc->content);
-		temp_here_doc = temp_here_doc->next;
-	}
-	ft_lstclear(&mini->here_doc_files, free);
-}*/
-
 int	parse(t_args *args, t_mini *mini)
 {
-	int			i;
-	t_params	*temp;
-	t_bool		heredoc_found;
+	int		i;
+	t_bool	heredoc_found;
 
 	heredoc_found = t_false;
-	printf("You entered: %s\n", args->input);
 	if (ft_tokenize(args, mini) == ERR)
 		return (ERR);
-	printf("argc ==> %i\n", args->argc);
-//	if (ft_built_ins(args, mini) == 1)
-//		return (OK);
 	i = 0;
 	while (i < args->argc)
 	{
 		if (args->args[i])
 		{
-			//printf("arg[%d]: %s\n", i, args->args[i]);
-			// printf("args->args[%i] ==> %s\n", i, args->args[i]);
-			// printf("args->quotes[%i] ==> %i\n", i, args->quotes[i]);
 			add_argument_to_list(args, &i, &heredoc_found);
 			free(args->args[i]);
 			args->args[i] = NULL;
 			args->quotes[i] = t_false;
-
 		}
 		i++;
 	}
-	//switch_to_delimiter(args);
 	update_last_command_env_var(args);
 	new_red_exe(args, mini);
 	close_inf_outf(mini);
-	printf("Parsed arguments:\n");
-	temp = args->params;
-	while (temp != NULL)
-	{
-		//printf("arg[%d]: %s,\t\ttype: %d,\t\tquotes: %i\n", temp->argc, temp->content, temp->type, temp->quotes);
-		printf("arg[%d]: %s", temp->argc, temp->content);
-		if (temp->type == CMD){printf("\ttype: %s", "CMD");}
-		else if (temp->type == INFILE){printf("\ttype: %s", "INFILE");}
-		else if (temp->type == HERE_DOC){printf("\ttype: %s", "HERE_DOC");}
-		else if (temp->type == OUTFILE){printf("\ttype: %s", "OUTFILE");}
-		else if (temp->type == APPEND){printf("\ttype: %s", "APPEND");}
-		else if (temp->type == PIPE){printf("\ttype: %s", "PIPE");}
-		else if (temp->type == PARAMS){printf("\ttype: %s", "PARAMS");}
-		else if (temp->type == BUILTING){printf("\ttype: %s", "BUILTING");}
-		else if (temp->type == DELIMITER){printf("\ttype: %s", "DELIMITER");}
-		if (temp->quotes == t_true){printf("\tquotes: %s\n", "TRUE");}
-		else if (temp->quotes == t_false){printf("\tquotes: %s\n", "FALSE");}
-		temp = temp->next;
-	}
 	return (OK);
 }
-/*
-command < infile | command > outfile
-command < infile
-expoort empieza por letra= algo
-*/
