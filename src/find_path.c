dd98563029d6b3fe15079e039d145bc653986196
Ines Sainz Ramon <isainz-r@c1r11s2.42madrid.com>
2024-10-02 15:10:59 +0200

    protect strchr if kid[0] does not exist

--- src/find_path.c
+++ src/find_path.c
@@ -53,7 +53,8 @@ char	*get_path_command(char **kid, char **env, char *path_mid)
 	char	*path;
 	char	**path_list;
 
-	if (ft_strchr(kid[0], '/'))
+	//ft_dprintf(2, "kid ==> %s")
+	if (kid[0] != NULL && ft_strchr(kid[0], '/'))
 		return (path_included(kid));
 	i = -1;
 	while (env[++i])
