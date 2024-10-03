b306d6f901a06a1783c7ffbf40d0ba6b0b8addbe
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-03 15:17:11 +0200

    Modified to return 0 value

--- src/built-ins/ft_cd.c
+++ src/built-ins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 14:20:47 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/09/23 14:23:48 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/10/03 11:36:35 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static void	update_oldpwd(char *buffer, char *output, t_mini *mini)
 	free(buffer);
 }
 
-static void	cd_arg(char **args, t_mini *mini)
+static int	cd_arg(char **args, t_mini *mini)
 {
 	char	*buffer;
 	char	*output;
@@ -55,17 +55,18 @@ static void	cd_arg(char **args, t_mini *mini)
 	if (chdir(args[1]) == -1)
 	{
 		perror("minishell: cd");
-		ft_export_env("?=1", mini);
-		return ;
+		//ft_export_env("?=1", mini);
+		return (1);
 	}
 	else
 	{
 		update_oldpwd(buffer, output, mini);
 		update_pwd(buffer, output, path, mini);
 	}
+	return (0);
 }
 
-static void	cd_home(t_mini *mini)
+static int	cd_home(t_mini *mini)
 {
 	char	*output;
 	char	*path;
@@ -75,8 +76,8 @@ static void	cd_home(t_mini *mini)
 	if (home != NULL && chdir(home) == -1)
 	{
 		perror("minishell: cd");
-		ft_export_env("?=1", mini);
-		return ;
+		//ft_export_env("?=1", mini);
+		return (1);
 	}
 	else
 	{
@@ -91,26 +92,29 @@ static void	cd_home(t_mini *mini)
 		printf("DB: PWD => %s\n", output);
 		ft_export_env(output, mini);
 		free(output);
-		ft_export_env("?=0", mini);
-		return ;
+		//ft_export_env("?=0", mini);
+		return (0);
 	}
 }
 
-void	ft_built_cd(char **args, t_mini *mini)
+int	ft_built_cd(char **args, t_mini *mini)
 {
 	int	argc;
+	int	exit_status;
 
 	argc = 0;
+	exit_status = 0;
 	while (args[argc])
 		argc++;
 	if (argc > 2)
 	{
 		printf("minishell: cd: too many arguments\n");
-		ft_export_env("?=1", mini);
-		return ;
+		//ft_export_env("?=1", mini);
+		return (1);
 	}
 	else if (argc == 1)
-		cd_home(mini);
+		exit_status = cd_home(mini);
 	else if (argc == 2)
-		cd_arg(args, mini);
+		exit_status = cd_arg(args, mini);
+	return (exit_status);
 }
