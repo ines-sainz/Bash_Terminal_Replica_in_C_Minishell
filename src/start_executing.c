9e8eecdcbef2de49aee0c73dbee457c2cd42cdc5
BishopVK <danielgraindorge@gmail.com>
2024-10-03 22:45:19 +0200

    Updated ft_built_unset() prototype to return int value to update exit_status

--- src/start_executing.c
+++ src/start_executing.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 12:13:15 by isainz-r          #+#    #+#             */
-/*   Updated: 2024/10/03 22:33:30 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/10/03 22:43:46 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	check_built_ins(char **command, t_execution *iter_exe,
 	else if (ft_strncmp(command[0], "export", len) == 0 && len == 6)
 		exit_status = ft_built_export(command, mini);
 	else if (ft_strncmp(command[0], "unset", len) == 0 && len == 5)
-		ft_built_unset(command, mini);
+		exit_status = ft_built_unset(command, mini);
 	else if (ft_strncmp(command[0], "env", len) == 0 && len == 3)
 		exit_status = ft_built_env(command, mini);
 	else if (ft_strncmp(command[0], "exit", len) == 0 && len == 4)
