61e186052dd59c57d946887408f8eeb31ef1dd03
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-03 15:19:39 +0200

    Captured built-ins return value to update 0

--- src/start_executing.c
+++ src/start_executing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   start_executing.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 12:13:15 by isainz-r          #+#    #+#             */
-/*   Updated: 2024/09/21 19:52:45 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/10/03 11:36:13 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 int	check_built_ins(char **command, t_execution *iter_exe,
 	t_mini *mini, t_args *args)
 {
-	int	len;
+	int		len;
+	int		exit_status;
 
+	exit_status = 0;
 	dup_redirections(iter_exe);
 	close_restant_fds(iter_exe, mini);
 	len = ft_strlen(command[0]);
 	if (ft_strncmp(command[0], "echo", len) == 0 && len == 4)
 		ft_built_echo(command);
 	else if (ft_strncmp(command[0], "cd", len) == 0 && len == 2)
-		ft_built_cd(command, mini);
+		exit_status = ft_built_cd(command, mini);
 	else if (ft_strncmp(command[0], "pwd", len) == 0 && len == 3)
 		ft_built_pwd(command, mini);
 	else if (ft_strncmp(command[0], "export", len) == 0 && len == 6)
@@ -34,7 +36,7 @@ int	check_built_ins(char **command, t_execution *iter_exe,
 		ft_print_env(mini);
 	else if (ft_strncmp(command[0], "exit", len) == 0 && len == 4)
 		ft_built_exit(args, command, mini);
-	return (0);
+	return (exit_status);
 }
 
 int	be_built_ins(char **command)
@@ -62,6 +64,7 @@ int	be_built_ins(char **command)
 void	create_fork(t_execution *iter_exe, t_mini *mini, t_args *args)
 {
 	pid_t	pid;
+	int		exit_status;
 
 	pid = fork();
 	if (pid == -1)
@@ -73,10 +76,11 @@ void	create_fork(t_execution *iter_exe, t_mini *mini, t_args *args)
 	{
 		if (be_built_ins(iter_exe->command) == 1)
 		{
-			check_built_ins(iter_exe->command, iter_exe, mini, args);
+			exit_status = check_built_ins(iter_exe->command,
+					iter_exe, mini, args);
 			close(mini->standard_fds[0]);
 			close(mini->standard_fds[1]);
-			exit (0);
+			exit (exit_status);
 		}
 		else
 			execute(iter_exe, mini, args);
@@ -94,6 +98,11 @@ void	close_fds(t_execution *iter_exe)
 int	start_executing(t_execution *iter_exe, int status,
 	t_mini *mini, t_args *args)
 {
+	int		exit_status;
+	char	*exit_status_itoa;
+	char	*exit_status_str;
+
+	exit_status = 0;
 	while (iter_exe != NULL)
 	{
 		if (iter_exe->inf_pipe < 0 || iter_exe->outf_pipe < 0)
@@ -103,15 +112,25 @@ int	start_executing(t_execution *iter_exe, int status,
 			continue ;
 		}
 		if (mini->n_commands == 1)
+		{
 			if (be_built_ins(iter_exe->command) == 1)
-				return (check_built_ins(iter_exe->command, iter_exe,
-						mini, args));
+			{
+				exit_status = check_built_ins(iter_exe->command, iter_exe,
+						mini, args);
+				exit_status_itoa = ft_itoa(exit_status);
+				exit_status_str = ft_strjoin("?=", exit_status_itoa);
+				free(exit_status_itoa);
+				ft_export_env(exit_status_str, mini);
+				free(exit_status_str);
+				return (0);
+			}
+		}
 		create_fork(iter_exe, mini, args);
 		close_fds(iter_exe);
 		iter_exe = iter_exe->next;
 	}
 	while (waitpid(-1, &status, 0) != -1)
 		continue ;
-	// ft_export_env("?=55", mini); Actualizar para Built-ins y execves
+	//ft_export_env("?=55", mini); //Actualizar para Built-ins y execves
 	return (WEXITSTATUS(status));
 }
