48fc6518e7f4cd7e56df85c24dcaaa888665b662
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-09 15:32:48 +0200

    Trying to fix long long int overflow

--- src/built-ins/ft_exit.c
+++ src/built-ins/ft_exit.c
@@ -6,17 +6,56 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:40:06 by danjimen &        #+#    #+#             */
-/*   Updated: 2024/10/08 14:39:58 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/10/09 15:32:10 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	is_valid_number(const char *str, long long int *result)
+{
+	int			negative;
+	long long	value;
+	int			digit;
+
+	negative = 0;
+	value = 0;
+	if (*str == '-')
+	{
+		negative = 1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return ERR; // No es un número válido
+		digit = *str - '0';
+		// Comprobar desbordamiento
+		if (value > (LLONG_MAX - digit) / 10)
+			return (ERR); // Desbordamiento
+		value = value * 10 + digit;
+		str++;
+	}
+	// Aplicar signo
+	if (negative)
+		value = -value;
+	// Comprobar límites
+	if (value < LLONG_MIN || value > LLONG_MAX)
+		return (ERR); // Fuera de límites
+	*result = value;
+	return (OK); // Éxito
+}
+
 static int	its_only_numbers(char *str)
 {
-	int	i;
+	int			i;
+	long long	num_not_valid;
 
 	i = 0;
+	if (*str == '\0')
+		return (ERR);
 	while (str[i] != '\0')
 	{
 		if (i == 0 && str[i] == '-')
@@ -25,6 +64,12 @@ static int	its_only_numbers(char *str)
 			return (ERR);
 		i++;
 	}
+	// LLONG_MAX 9223372036854775808 LLONG_MIN -9223372036854775809
+	if (!is_valid_number(str, &num_not_valid))
+		return (ERR);
+	/* num_not_valid = ft_custom_atoll(str);
+	if ((unsigned long long int)num_not_valid == ULLONG_MAX)
+		return (ERR); */
 	return (OK);
 }
 
