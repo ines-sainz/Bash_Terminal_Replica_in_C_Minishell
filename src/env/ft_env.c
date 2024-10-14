32da2df845d18fd51966a485c77214745404bbf3
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-14 08:53:17 +0200

    Moved unset functions to ft_env_unset.c. Created ft_update_existing_node() function whit a part of ft_existing_node() to fix the norminette

--- src/env/ft_env.c
+++ src/env/ft_env.c
@@ -6,59 +6,34 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 09:42:02 by isainz-r          #+#    #+#             */
-/*   Updated: 2024/10/01 15:47:18 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/10/14 08:46:30 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Cuento con que solo me dan la variable que
-// quiere desetear, no el contenido también
-static int	ft_to_unset(t_env *prev, char *unset, t_mini *mini)
+static int	ft_update_existing_node(char *new_env, t_mini *mini,
+	char *name, int *equal)
 {
-	if (!ft_strncmp(mini->env_iter->variable,
-			mini->env_first_node->variable,
-			ft_strlen(mini->env_first_node->variable)))
-	{
-		mini->env_first_node = mini->env_first_node->next;
-		free(mini->env_iter->variable);
-		free(mini->env_iter->content);
-		free(mini->env_iter);
-		return (0);
-	}
-	while (prev != NULL && ft_strncmp(prev->next->variable,
-			unset, ft_strlen(unset)))
-		prev = prev->next;
-	if (mini->env_iter->next)
-		prev->next = mini->env_iter->next;
-	else
-		prev->next = NULL;
-	free(mini->env_iter->variable);
-	free(mini->env_iter->content);
-	free(mini->env_iter);
-	return (0);
-}
-
-void	ft_unset_env(char *unset, t_mini *mini)
-{
-	t_env	*prev;
-
 	mini->env_iter = mini->env_first_node;
-	if (ft_strcmp(unset, "_") == 0 || ft_strcmp(unset, "?") == 0)
-		return ;
 	while (mini->env_iter != NULL)
 	{
-		if (!ft_strncmp(unset, mini->env_iter->variable, ft_strlen(unset)))
+		if (ft_strcmp(name, mini->env_iter->variable) == 0 && *equal == 0)
+			return (free(name), OK);
+		if (ft_strcmp(name, mini->env_iter->variable) == 0 && *equal != 0)
 		{
-			prev = mini->env_first_node;
-			ft_to_unset(prev, unset, mini);
-			return ;
+			free(name);
+			free(mini->env_iter->content);
+			mini->env_iter->content = ft_substr(new_env, *equal + 1,
+					ft_strlen(new_env) - *equal + 1);
+			return (OK);
 		}
 		mini->env_iter = mini->env_iter->next;
 	}
+	return (ERR);
 }
 
-int	ft_existing_node(char *new_env, t_mini *mini)
+static int	ft_existing_node(char *new_env, t_mini *mini)
 {
 	int		i;
 	int		equal;
@@ -76,20 +51,8 @@ int	ft_existing_node(char *new_env, t_mini *mini)
 		name = ft_substr(new_env, 0, equal);
 	else
 		name = ft_substr(new_env, 0, ft_strlen(new_env));
-	mini->env_iter = mini->env_first_node;
-	while (mini->env_iter != NULL)
-	{
-		if (ft_strcmp(name, mini->env_iter->variable) == 0 && equal == 0)
-			return (free(name), OK);
-		if (ft_strcmp(name, mini->env_iter->variable) == 0 && equal != 0)
-		{
-			free(name);
-			free(mini->env_iter->content);
-			mini->env_iter->content = ft_substr(new_env, equal + 1, ft_strlen(new_env) - equal + 1);
-			return (OK);
-		}
-		mini->env_iter = mini->env_iter->next;
-	}
+	if (ft_update_existing_node(new_env, mini, name, &equal) == OK)
+		return (OK);
 	return (free(name), ERR);
 }
 
@@ -99,13 +62,9 @@ void	ft_export_env(char *new_env, t_mini *mini)
 
 	if (ft_existing_node(new_env, mini) == OK)
 		return ;
-	//COMPROBAR EL CONTENIDO, POR SI YA EXISTE, ANTES DE CREAR EL NODO
 	new_node = env_new(new_env);
 	if (!new_node)
-	{
-		free_env(mini);
-		return ;
-	}
+		return (free_env(mini), (void)0);
 	mini->env_iter = mini->env_first_node;
 	while (mini->env_iter != NULL)
 	{
