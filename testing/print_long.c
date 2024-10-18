ae08a1342304c7da40a4179206b630fb7ca32f6f
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-18 14:25:34 +0200

    Created file for long lines printf testing

---
+++ testing/print_long.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_long.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 11:50:38 by danjimen &        #+#    #+#             */
+/*   Updated: 2024/10/18 11:54:43 by danjimen &       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main(int argc, char const *argv[])
+{
+	printf("abcdefghijklmnopqrstuvwxyz0123456789"
+		"abcdefghijklmnopqrstuvwxyz0123456789\n");
+	return (0);
+}
