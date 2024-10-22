ecee2d6a2d4420dadbaedba5f1e65a78abfc8cec
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-22 12:24:44 +0200

    Added DB flag to empty line to remove whit Debug Scipt

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:15:16 by danjimen          #+#    #+#             */
-/*   Updated: 2024/10/22 12:19:21 by danjimen &       ###   ########.fr       */
+/*   Updated: 2024/10/22 12:24:13 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ static void	loop_args_to_create_list(t_args *args)
 int	parse(t_args *args, t_mini *mini)
 {
 	t_params	*temp; //DB
-
+	 //DB
 	if (ft_tokenize(args, mini) == ERR)
 		return (ERR);
 	loop_args_to_create_list(args);
