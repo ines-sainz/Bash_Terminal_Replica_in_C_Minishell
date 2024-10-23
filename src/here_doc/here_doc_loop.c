a865eb41e687bc0ca37c6086045fc76fd677b1c5
BishopVK <danielgraindorge@gmail.com>
2024-10-23 22:07:35 +0200

    Moved part of here_doc_loop() code to others functions

--- src/here_doc/here_doc_loop.c
+++ src/here_doc/here_doc_loop.c
@@ -6,11 +6,23 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 21:25:32 by danjimen          #+#    #+#             */
-/*   Updated: 2024/10/23 21:33:39 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/10/23 22:06:56 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static char	*initialize_here_doc(t_params *param, t_args *here_doc, t_mini *mini)
+{
+	char	*eof;
+
+	eof = param->content;
+	ft_bzero(here_doc, sizeof(t_args));
+	here_doc->mini = mini;
+	signal(SIGINT, signal_here_doc);
+	g_signal_received = 0;
+	return (eof);
+}
 
 static char	*ft_get_eof(char *eof)
 {
@@ -23,36 +35,40 @@ static char	*ft_get_eof(char *eof)
 	return (eof_fin);
 }
 
+static int	initialize_here_doc_loop(t_args	*here_doc, char *eof, t_params *param, char **eof_fin)
+{
+	if (g_signal_received == SIGINT)
+		return (ERR);
+	here_doc->arg = readline("> ");
+	if (!here_doc->arg)
+	{
+		ft_dprintf(2, "minishell: warning: here-document delimited by"
+			" end-of-file (wanted `EOF')\n");
+		return (ERR);
+	}
+	if (g_signal_received == SIGINT)
+	{
+		free(here_doc->arg);
+		return (ERR);
+	}
+	if (eof[0] == '$' && ft_strlen(eof) >= 1 && param->quotes == t_true)
+		*eof_fin = ft_substr(eof, 1, ft_strlen(eof));
+	else
+		*eof_fin = ft_get_eof(eof);
+	return (OK);
+}
+
 int	here_doc_loop(int fd, t_params *param, t_mini *mini)
 {
 	t_args	here_doc;
 	char	*eof;
 	char	*eof_fin;
 
-	eof = param->content;
-	ft_bzero(&here_doc, sizeof(t_args));
-	here_doc.mini = mini;
-	signal(SIGINT, signal_here_doc);
-	g_signal_received = 0;
+	eof = initialize_here_doc(param, &here_doc, mini);
 	while (1)
 	{
-		if (g_signal_received == SIGINT)
+		if (initialize_here_doc_loop(&here_doc, eof, param, &eof_fin) == ERR)
 			break ;
-		here_doc.arg = readline("> ");
-		if (!here_doc.arg)
-		{
-			ft_dprintf(2, "minishell: warning: here-document delimited by end-of-file (wanted `EOF')\n");
-			break ;
-		}
-		if (g_signal_received == SIGINT)
-		{
-			free(here_doc.arg);
-			break ;
-		}
-		if (eof[0] == '$' && ft_strlen(eof) >= 1 && param->quotes == t_true)
-			eof_fin = ft_substr(eof, 1, ft_strlen(eof));
-		else
-			eof_fin = ft_get_eof(eof);
 		if ((eof[0] == '\0' && here_doc.arg[0] == '\0')
 			|| (!ft_strncmp(eof_fin, here_doc.arg, ft_strlen(here_doc.arg))
 			&& ft_strlen(here_doc.arg) > 0) || (eof[0] == '$'
