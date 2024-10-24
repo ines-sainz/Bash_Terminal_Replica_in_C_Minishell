34f61a6f869b699b8109df2007f2d9549bece516
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-24 13:16:08 +0200

    Norminette fixed

--- src/here_doc/here_doc.c
+++ src/here_doc/here_doc.c
@@ -3,93 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 12:25:19 by isainz-r          #+#    #+#             */
-/*   Updated: 2024/10/23 21:32:38 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/10/24 13:15:41 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-// char	*ft_get_eof(char *eof)
-// {
-// 	char	*eof_fin;
-
-// 	eof_fin = (char *)malloc(ft_strlen(eof) + 2);
-// 	ft_strlcpy(eof_fin, eof, ft_strlen(eof) + 1);
-// 	eof_fin[ft_strlen(eof)] = '\n';
-// 	eof_fin[ft_strlen(eof) + 1] = '\0';
-// 	return (eof_fin);
-// }
-
-// int	ft_write_temp(int fd, t_params *param, t_mini *mini)
-// {
-// 	t_args	here_doc;
-// 	char	*eof;
-// 	char	*eof_fin;
-
-// 	eof = param->content;
-// 	ft_bzero(&here_doc, sizeof(t_args));
-// 	here_doc.mini = mini;
-// 	signal(SIGINT, signal_here_doc);
-// 	g_signal_received = 0;
-// 	while (1)
-// 	{
-// 		if (g_signal_received == SIGINT)
-// 			break ;
-// 		here_doc.arg = readline("> ");
-// 		if (!here_doc.arg)
-// 		{
-// 			ft_dprintf(2, "minishell: warning: here-document delimited by end-of-file (wanted `EOF')\n");
-// 			break ;
-// 		}
-// 		if (g_signal_received == SIGINT)
-// 		{
-// 			free(here_doc.arg);
-// 			break ;
-// 		}
-// 		if (eof[0] == '$' && ft_strlen(eof) >= 1 && param->quotes == t_true)
-// 			eof_fin = ft_substr(eof, 1, ft_strlen(eof));
-// 		else
-// 			eof_fin = ft_get_eof(eof);
-// 		if ((eof[0] == '\0' && here_doc.arg[0] == '\0')
-// 			|| (!ft_strncmp(eof_fin, here_doc.arg, ft_strlen(here_doc.arg))
-// 			&& ft_strlen(here_doc.arg) > 0) || (eof[0] == '$'
-// 			&& ft_strlen(eof) == 1 && param->quotes == t_true
-// 			&& here_doc.arg[0] == '\0'))
-// 		{
-// 			free(here_doc.arg);
-// 			free(eof_fin);
-// 			break ;
-// 		}
-// 		if (param->quotes == t_false)
-// 		{
-// 			expander(&here_doc, mini);
-// 			printf("DB: expanded_arg => %s\n", here_doc.result);
-// 			write(fd, here_doc.result, ft_strlen(here_doc.result));
-// 			write(fd, "\n", 1);
-// 			free(here_doc.arg);
-// 			free(here_doc.result);
-// 			free(eof_fin);
-// 		}
-// 		else
-// 		{
-// 			write(fd, here_doc.arg, ft_strlen(here_doc.arg));
-// 			write(fd, "\n", 1);
-// 			free(here_doc.arg);
-// 			free(eof_fin);
-// 		}
-// 	}
-// 	signal(SIGINT, signal_sigint);
-// 	if (g_signal_received == SIGINT)
-// 	{
-// 		g_signal_received = 0;
-// 		close(fd);
-// 		return (-2);
-// 	}
-// 	return (OK);
-// }
 
 static char	*get_here_doc_file_name(void)
 {
