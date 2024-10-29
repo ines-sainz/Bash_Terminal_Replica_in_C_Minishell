f2601dd53a6b83c2091a0457238fc583b1c13efc
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-29 08:01:14 +0100

    Added obtain_last_status() function to detect signals exit during the execution

--- src/main/signals_and_history.c
+++ src/main/signals_and_history.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals_and_history.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen & isainz-r <danjimen & isainz-    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 20:17:04 by danjimen          #+#    #+#             */
-/*   Updated: 2024/10/23 20:39:13 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/10/29 07:57:09 by danjimen &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,18 @@ void	signal_here_doc(int sig)
 		rl_replace_line("", 1);
 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
 	}
+}
+
+int	obtain_last_status(int *status)
+{
+	if (WTERMSIG(*status) != 0)
+	{
+		if (WTERMSIG(*status) == 3)
+			ft_dprintf(2, "Quit (core dumped)");
+		ft_dprintf(2, "\n");
+		return (WTERMSIG(*status) + 128);
+	}
+	return (WEXITSTATUS(*status));
 }
 
 void	histcontrol(t_args *args)
